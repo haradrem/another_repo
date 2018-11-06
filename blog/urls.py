786dfbf17caff28ffc142d4f07189c475fd64102
egosha <egorovsd93@gmail.com>
2018-11-06 16:16:23 +0400

    122

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    path('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path('^post/new/$', views.post_new, name='post_new')
 ]
